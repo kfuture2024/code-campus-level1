30a6650f05541ec92a8f69c1bc4a9ee190631156
kirangaonkar <kirangaonkar016@gmail.com>
2024-03-10 20:44:14 +0100

    Day 3 tasks for the challenge

---
+++ index.js
@@ -0,0 +1,40 @@
+// Execution of first JS code
+console.log("Hello Wildr!!");
+// - Variables and Data types in Javascript , Refer here: [https://javascript.info/first-steps](https://javascript.info/first-steps)
+
+// - Print the message in the terminal **My name is {your name}** , Replace {your name} with actual name.
+console.log("My name is Kiran");
+// - Declare a variable and assign your age (any number)
+let age = 30;
+// - Print the value
+console.log(age);
+// - Declare another variable and assign name
+let myName = "Kiran";
+// - Print the value
+console.log(myName);
+// - Add 2 numbers, print both numbers and the sum
+let a = 2;
+let b = 3;
+let c = a + b;
+
+console.log(a, b, c);
+// - Add 2 strings, print both strings and the conjunction
+let firstName = "Kiran";
+let lastName = "Gaonkar";
+let fullName = firstName + lastName;
+console.log(firstName, lastName, fullName);
+// - Perform all basic Math operations and print , Refer: [https://javascript.info/operators#maths](https://javascript.info/operators#maths)
+// Subtraction
+let d = b-a
+// division
+let e = b/a
+// multiplication
+let f = b*a
+// modulus
+let g = b%a
+
+console.log(d,e,f,g)
+
+let string1 = "test"
+
+console.log(string1*6)
